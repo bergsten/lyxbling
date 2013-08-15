c909af867888317ae73d14af56993302f7a29c7b
mats.bergsten <mats.bergsten@Mats-PC.occidentdue.se>
2013-08-15 13:46:09 +0200

    Created lyxbling child theme files for the new Headway main theme.

--- wp-content/themes/lyxbling/functions.php
+++ wp-content/themes/lyxbling/functions.php
@@ -1,6 +1,12 @@
 <?php
- 
-// Load the textdomain for translation
-load_child_theme_textdomain( 'woothemes' );
- 
+
+/*
+ * Insert all post types in RSS feeds.
+ */
+function myfeed_request($qv) {
+	if (isset($qv['feed']))
+		$qv['post_type'] = get_post_types();
+	return $qv;
+}
+add_filter('request', 'myfeed_request');
 ?>
